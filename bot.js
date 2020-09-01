06e36b99ba1807b66f020629ad57154e33b372d7
Geoff Lamperd <glamperd@gmail.com>
2020-09-01 13:09:27 +1000

    log

--- bot.js
+++ bot.js
@@ -430,7 +430,8 @@ const getEmoji = (guildName, emojiName) => {
         // Set reaction emoji
         const guild = getGuild(guildName);
         if (guild) {
-            return guild.emojis.cache.find(emoji => emoji.name === emojiName);
+            const emoji = guild.emojis.cache.find(emoji => emoji.name === emojiName);
+            console.log(`Found emoji ${emoji.toString()} id ${emoji.identifier}`);
         }
         return false;
 }
